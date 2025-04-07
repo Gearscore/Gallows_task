9be7b1f9e196547dcd9075aeccdce1dd936f08b6
Evgeniy_Zhelak <Ev-ZHelak@mail.ru>
2025-04-07 17:31:28 +0700

    uodate data

--- release/make.bat
+++ release/make.bat
@@ -1,5 +1,13 @@
+@echo off
+REM Активация виртуального окружения
 call ..\venv\Scripts\activate.bat
+
+REM Проверка установленных зависимостей
 python.exe -m pip list
 pause
+
+REM Сборка exe-файла через PyInstaller
 pyinstaller -F --icon=app.ico --name=GameGallows ..\main.py
+
+REM Пауза для просмотра результатов
 pause
